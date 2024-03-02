85d99574cf947299cd0d86f72d732358452fe4b1
hager-dot <hagerelhwarii6633@gmail.com>
2024-03-02 13:35:10 +0200

    🐛 Fix song name bug on changeAudio

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-var a = document.getElementsByTagName("audio")[0]
-console.dir(a)
-//console.log(a.canPlayType("audio/ogg"));
+let audio = document.getElementsByClassName("audio")[0];
+console.dir(audio);
 
-// array of objects of available songs
-var songs = [
+// array available songs
+const songs = [
   {
     id: 1,
     coverImage: "/images/stay.png",
@@ -28,61 +27,61 @@ var songs = [
     audioSrc: "/music/music_RatherBe.mp3",
     name: "Rather Be_Clean Bandit",
   },
-]
+];
 
 window.addEventListener("load", function () {
-  //   console.log("loaded")
-  document.getElementById("rangeAudio").max = a.duration
-})
+  document.getElementById("rangeAudio").max = audio.duration;
+});
 
-function changeAudio(rangeAudio) {
-  a.currentTime = rangeAudio.valueAsNumber
-}
+// function changeAudio(rangeAudio) {
+//   audio.currentTime = rangeAudio.valueAsNumber;
+// }
 
 function muteAudio() {
-  a.muted = !a.muted
+  audio.muted = !audio.muted;
 }
 
-function changeVolume(volumeRange) {
-  a.volume = volumeRange.valueAsNumber
-}
+// function changeVolume(volumeRange) {
+//   audio.volume = volumeRange.valueAsNumber;
+// }
 
 function playAudio() {
-  a.play()
+  audio.play();
 }
 
-function pauseAudio() {
-  a.pause()
+function TogglePauseAudio() {
+  if (audio.paused) {
+    audio.play();
+  } else {
+    audio.pause();
+  }
 }
 
 function stopAudio() {
-  a.load()
-  a.pause()
+  audio.load();
+  audio.pause();
 }
 
-var current_song = document.getElementById("song-num")
-var songCover = document.getElementById("cover-img")
-var audioSrc = document.getElementById("audio-src")
-var songName = document.getElementById("song-name")
+const current_song = document.getElementById("song-num");
+const songCover = document.getElementById("cover-img");
+const audioSrc = document.getElementById("audio-src");
+const songName = document.getElementById("song-name");
 
 function changeAudio() {
-  //   debugger
-  stopAudio()
-  var current_song_num = current_song.innerText
-  // var current_song_Name = songName.innerText
+  stopAudio();
+  let current_song_num = current_song.innerText;
   if (current_song_num != "4") {
-    var nextSong = songs[parseInt(current_song_num)]
+    var nextSong = songs[parseInt(current_song_num)];
   } else {
-    nextSong = songs[0]
+    nextSong = songs[0];
   }
-
-  changeSongSrc(nextSong)
+  changeSongSrc(nextSong);
 }
 
 function changeSongSrc(nextSong) {
-  songCover.src = nextSong.coverImage
-  audioSrc.src = nextSong.audioSrc
-  current_song.innerHTML = nextSong.id
-  songName.innerHtml = nextSong.id
-  playAudio()
+  songCover.src = nextSong.coverImage;
+  audioSrc.src = nextSong.audioSrc;
+  current_song.innerHTML = nextSong.id;
+  songName.innerHTML = nextSong.name;
+  playAudio();
 }
